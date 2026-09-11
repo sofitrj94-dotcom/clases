cb1a56ccd7eabe4f4da5a17beebcc449e0730234
sofiatrujillo94 <sofitrj94@gmail.com>
2026-09-11 12:17:26 -0500

    Actividad calificable POO

--- index.js
+++ index.js
@@ -1,61 +1,71 @@
 class Animal {
-    nombre
-    raza
-    especie
-    edad
-
+    nombre;
+    raza;
+    especie;
+    edad;
+    
     constructor(nombre, raza, especie, edad) {
-        this.nombre = nombre
-        this.raza = raza
-        this.especie = especie
-        this.edad = edad
+        this.nombre = nombre;
+        this.raza = raza;
+        this.especie = especie;
+        this.edad = edad;
     }
-    registrarNombre(nuevonombre) {
-        this.nombre = nuevonombre
+    
+    registrarNombre(nuevoNombre) {
+        this.nombre = nuevoNombre;
     }
     registrarRaza(nuevaRaza) {
-        this.raza = nuevaRaza
+        this.raza = nuevaRaza;
     }
     registrarEspecie(nuevaEspecie) {
-        this.especie = nuevaEspecie
+        this.especie = nuevaEspecie;
     }
     registrarEdad(nuevaEdad) {
-        this.edad = nuevaEdad
+        this.edad = nuevaEdad;
     }
+    
+    // Corregido el nombre del método y añadido espacio
     mostrarInformacion() {
-        console.log(this.nombre + this.raza)
+        console.log(this.nombre + " " + this.raza);
     }
 }
 
 class Propietario {
-    nombrePropietario
-    Direccion
-    Telefono
-    constructor(nombre, direccion, telefono) {
-        this.nombrePropietario = nombre
-        this.Direccion = direccion
-        this.telefono = telefono
-        this.mascota = []
+    nombrePropietario;
+    Direccion;
+    Telefono;
+    
+    constructor(nombrePropietario, Direccion, Telefono) {
+        this.nombrePropietario = nombrePropietario;
+        this.Direccion = Direccion;
+        this.Telefono = Telefono;
+        this.mascota = [];
     }
+    
     registrarNombre(nuevoNombre) {
-        this.nombrePropietario = nuevoNombre
+        this.nombrePropietario = nuevoNombre;
     }
     registrarDireccion(nuevaDireccion) {
-        this.Direccion = nuevaDireccion
+        this.Direccion = nuevaDireccion;
     }
-    registrarTelefono(nuevaTelefono) {
-        this.telefono = nuevaTelefono
+    registrarTelefono(nuevoTelefono) {
+        this.Telefono = nuevoTelefono;
     }
-    registrarMascota(nuevamascota) {
-        //pop,unshift,shift,push
-        this.mascota.push(nuevamascota)
+    registrarMascota(nuevaMascota) {
+        this.mascota.push(nuevaMascota);
     }
-    mostrarInformacion(){
-        console.log("mascota: "+this.mascota+"Propietario: "+this.nombrePropietario)
+    
+    mostrarInformacion() {
+        // Corregido para acceder al .nombre de la mascota y dar formato
+        console.log("Mascota: " + this.mascota[0].nombre + " | Propietario: " + this.nombrePropietario);
     }
 }
-let colmillo = new Animal("colmillo", "pitbull", "perro", "123")
-colmillo.mostrarInformacion()
-let karen = new Propietario("Karen", "123", "1111")
-karen.registrarMascota(colmillo)
-karen.mostrarInformacion()
+
+let colmillo = new Animal("Colmillo", "Pitbull", "perro", 12);
+colmillo.mostrarInformacion(); 
+// Imprime: Colmillo Pitbull
+
+let alexa = new Propietario("Alexa", "123", "1111");
+alexa.registrarMascota(colmillo);
+alexa.mostrarInformacion(); 
+// Imprime: Mascota: Colmillo | Propietario: Alexa
