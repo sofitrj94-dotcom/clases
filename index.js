72ce51451ab8ab6e5f2e95ec074d2f2ea6799899
sofiatrujillo94 <sofitrj94@gmail.com>
2026-09-11 11:03:30 -0500

    Quinto commit

--- index.js
+++ index.js
@@ -1,9 +1,61 @@
-console.log("hola")
+class Animal {
+    nombre
+    raza
+    especie
+    edad
 
+    constructor(nombre, raza, especie, edad) {
+        this.nombre = nombre
+        this.raza = raza
+        this.especie = especie
+        this.edad = edad
+    }
+    registrarNombre(nuevonombre) {
+        this.nombre = nuevonombre
+    }
+    registrarRaza(nuevaRaza) {
+        this.raza = nuevaRaza
+    }
+    registrarEspecie(nuevaEspecie) {
+        this.especie = nuevaEspecie
+    }
+    registrarEdad(nuevaEdad) {
+        this.edad = nuevaEdad
+    }
+    mostrarInformacion() {
+        console.log(this.nombre + this.raza)
+    }
+}
 
-console.log("h")
-console.log("o")
-console.log("l")
-console.log("a")
-
-
+class Propietario {
+    nombrePropietario
+    Direccion
+    Telefono
+    constructor(nombre, direccion, telefono) {
+        this.nombrePropietario = nombre
+        this.Direccion = direccion
+        this.telefono = telefono
+        this.mascota = []
+    }
+    registrarNombre(nuevoNombre) {
+        this.nombrePropietario = nuevoNombre
+    }
+    registrarDireccion(nuevaDireccion) {
+        this.Direccion = nuevaDireccion
+    }
+    registrarTelefono(nuevaTelefono) {
+        this.telefono = nuevaTelefono
+    }
+    registrarMascota(nuevamascota) {
+        //pop,unshift,shift,push
+        this.mascota.push(nuevamascota)
+    }
+    mostrarInformacion(){
+        console.log("mascota: "+this.mascota+"Propietario: "+this.nombrePropietario)
+    }
+}
+let colmillo = new Animal("colmillo", "pitbull", "perro", "123")
+colmillo.mostrarInformacion()
+let karen = new Propietario("Karen", "123", "1111")
+karen.registrarMascota(colmillo)
+karen.mostrarInformacion()
---
+++ push.js
@@ -0,0 +1,30 @@
+//push 
+
+let lista=[]
+lista.push("a")
+console.log(lista)
+lista.push("b")
+console.log(lista)
+
+//push es para agregar al final de un arreglo
+
+
+//unshift
+
+lista.unshift("c")
+console.log(lista)
+
+//unshift es para agregar al inicio de un arreglo
+
+
+//pop
+lista.pop()
+console.log(lista)
+
+//pop es para eliminar elementos al final 
+
+//shift 
+lista.shift()
+console.log(lista)
+
+//shift es para eliminar elementos al inicio
